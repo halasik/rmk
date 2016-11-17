937869848500e5ef9fd9cf8d753252c51c2ee190
halaspavel <p.halas@yudway.by>
2016-11-17 10:39:24 +0300

    Создан класс для работы с командами в базе данных, сделана структура согласно принципам SOLID

---
+++ Anonym/Anonym/Model/DataBase/MySql/DbCommandMySql.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Anonym.Model.DataBaseMySQL
+{
+    class DbCommandMySql : IDbCommand
+    {
+        private MySqlCommand command;
+
+        public string CommandText
+        {
+            get
+            {
+                return command.CommandText;
+            }
+
+            set
+            {
+                command.CommandText = value;
+            }
+        }
+
+        public int CommandTimeout
+        {
+            get
+            {
+                return command.CommandTimeout;
+            }
+
+            set
+            {
+                command.CommandTimeout = value;
+            }
+        }
+
+        public CommandType CommandType
+        {
+            get
+            {
+                return command.CommandType;
+            }
+
+            set
+            {
+                command.CommandType = value;
+            }
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return command.Connection;
+            }
+
+            set
+            {
+                command.Connection = (MySqlConnection)value;
+            }
+        }
+
+        public IDataParameterCollection Parameters
+        {
+            get
+            {
+                return command.Parameters;
+            }
+        }
+
+        public IDbTransaction Transaction
+        {
+            get
+            {
+                return command.Transaction;
+            }
+
+            set
+            {
+                command.Transaction = (MySqlTransaction)value;
+            }
+        }
+
+        public UpdateRowSource UpdatedRowSource
+        {
+            get
+            {
+                return command.UpdatedRowSource;
+            }
+
+            set
+            {
+                command.UpdatedRowSource = value;
+            }
+        }
+
+        public DbCommandMySql(MySqlConnection connection, string cmdText)
+        {
+            command = new MySqlCommand(cmdText, connection);
+        }
+
+        public void Cancel()
+        {
+            command.Cancel();
+        }
+
+        public IDbDataParameter CreateParameter()
+        {
+            return command.CreateParameter();
+        }
+
+        public void Dispose()
+        {
+            command.Dispose();
+        }
+
+        public int ExecuteNonQuery()
+        {
+            return command.ExecuteNonQuery();
+        }
+
+        public IDataReader ExecuteReader()
+        {
+            return command.ExecuteReader();
+        }
+
+        public IDataReader ExecuteReader(CommandBehavior behavior)
+        {
+            return command.ExecuteReader(behavior);
+        }
+
+        public object ExecuteScalar()
+        {
+            return command.ExecuteReader();
+        }
+
+        public void Prepare()
+        {
+            command.Prepare();
+        }
+    }
+}
---
+++ Anonym/Anonym/Model/DataBase/MySql/DbConnectionMySQL.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Anonym.Model.DataBaseMySQL
+{
+    class DbConnectionMySQL : IDbConnection
+    {
+
+        private MySqlConnection connection;
+       
+        public string ConnectionString
+        {
+            get
+            {
+                return connection.ConnectionString;
+            }
+
+            set
+            {
+                connection.ConnectionString = value;
+            }
+        }
+
+        public int ConnectionTimeout
+        {
+            get
+            {
+                return connection.ConnectionTimeout;
+            }
+        }
+
+        public string Database
+        {
+            get
+            {
+                return connection.Database;
+            }
+        }
+
+        public ConnectionState State
+        {
+            get
+            {
+                return connection.State;
+            }
+        }
+
+        public DbConnectionMySQL(string connectionString)
+        {
+            connection = new MySqlConnection(connectionString);         
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            return connection.BeginTransaction();
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            return connection.BeginTransaction(il);
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+
+        public void ChangeDatabase(string databaseName)
+        {
+            connection.ChangeDatabase(databaseName);
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            return connection.CreateCommand();
+        }
+
+        public void Open()
+        {
+           connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+        }
+
+    }
+}
